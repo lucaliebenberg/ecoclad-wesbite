6e1e4be200ccac7266e50b379a3937b86aac3fab
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-05-24 08:25:22 +0200

    OurServices page added

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -1,3 +1,5 @@
+import Footer from "../components/Footer";
+
 const OurServices = () => {
   return (
     <>
@@ -15,16 +17,14 @@ const OurServices = () => {
           </p>
         </div>
 
-        <div className="w-full h-auto">
-          <div className="flex flex-col ml-auto mr-auto md:flex-row md:justify-between">
+        <div className="w-full h-auto pl-[3.3rem] mt-[4rem]">
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
             <img
-              src="https://picsum.photos/200/300"
-              width="54%"
-              height="auto"
+              src="https://picsum.photos/550/320"
               alt=""
-              className="rounded-[12px]"
+              className="rounded-[12px] h-auto"
             />
-            <h2 className="text-[30px] font-semibold text-black">
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
               Clients Roof Design And Insulation
             </h2>
             <p className="text-[16px] font-regular text-black">
@@ -38,7 +38,93 @@ const OurServices = () => {
               specialists.
             </p>
           </div>
+
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
+            <img
+              src="https://picsum.photos/550/320"
+              alt=""
+              className="rounded-[12px] h-auto"
+            />
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
+              Roof Structure Comprehensive Design
+            </h2>
+            <p className="text-[16px] font-regular text-black">
+              Based on assessments, the development brief and a thorough
+              knowledge of what is available in the Market, EcoClad will design
+              a suitable solution. This will entail selecting the preferred
+              materials and structural design to suit the Client’s requirements.
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
+            <img
+              src="https://picsum.photos/550/320"
+              alt=""
+              className="rounded-[12px] h-auto"
+            />
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
+              Roof Material Specification
+            </h2>
+            <p className="text-[16px] font-regular text-black">
+              Based on an approved design – whether created by ourselves or a
+              third party consultant, EcoClad will draw up a full project plan
+              and specification. This to ensure the procurement of materials and
+              the construction process for the Project.
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
+            <img
+              src="https://picsum.photos/550/320"
+              alt=""
+              className="rounded-[12px] h-auto"
+            />
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
+              Roof Material Procurement Options
+            </h2>
+            <p className="text-[16px] font-regular text-black">
+              EcoClad will source and deliver materials strictly in accordance
+              with the project specification. We will ensure correct material
+              selection as well as availability since this dictates the price,
+              functional performance and life-span of the project.
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
+            <img
+              src="https://picsum.photos/550/320"
+              alt=""
+              className="rounded-[12px] h-auto"
+            />
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
+              Low Price Installation By Experienced Crew
+            </h2>
+            <p className="text-[16px] font-regular text-black">
+              EcoClad’s construction and installation teams work under strict
+              project management Supervision. All installations are subject to
+              management oversight throughout. Strict quality control is imposed
+              at all times and all installations are guaranteed accordingly.
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col ml-auto mr-auto md:flex-row md:justify-between pr-8">
+            <img
+              src="https://picsum.photos/550/320"
+              alt=""
+              className="rounded-[12px] h-auto"
+            />
+            <h2 className="text-[18px] pt-3 pb-3 md:text-[30px] font-semibold text-black">
+              Roof Maintenance Of Industrial, Commercial Complexes
+            </h2>
+            <p className="text-[16px] font-regular text-black">
+              All roofing and related structures will perform for many years. In
+              order to ensure this, EcoClad offers a full maintenance service
+              over and above their workmanship guarantee and Manufacturers
+              warranty for completed installations.
+            </p>
+          </div>
         </div>
+        <Footer />
       </div>
     </>
   );
