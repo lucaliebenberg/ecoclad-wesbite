082b253dba0c945b4714d00692a9e0e109e0c991
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-06-03 13:59:56 +0200

    updated whatweoffer section

--- src/components/WhatWeOffer.jsx
+++ src/components/WhatWeOffer.jsx
@@ -23,21 +23,39 @@ const WhatWeOffer = () => {
       <div className="flex flex-col md:flex-row md:justify-between pl-[3.3rem] md:flex-wrap gap-8 md:gap-8 w-[80%]">
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
           <img src={Consulting} alt="Consulting" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Consulting
+          </h2>
         </div>
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
-          <img src={SolutionDesign} alt="SolutionDesign" />
+          <img src={SolutionDesign} alt="Solution Design" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Solution Design
+          </h2>
         </div>
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
-          <img src={SolutionSpec} alt="SolutionSpec" />
+          <img src={SolutionSpec} alt="Solution Specification" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Solution Specification
+          </h2>
         </div>
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
           <img src={Procurement} alt="Procurement" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Procurement
+          </h2>
         </div>
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
           <img src={Installation} alt="Installation" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Installation
+          </h2>
         </div>
         <div className="w-[90%] h-auto md:w-[38%] md:h-[32%] rounded-[4px] shadow-md hover:scale-105 duration-300 ease-in-out transition-all">
           <img src={Maintenance} alt="Maintenance" />
+          <h2 className="text-[1.5rem] text-black font-semibold p-1.5">
+            Maintenance
+          </h2>
         </div>
       </div>
     </div>
